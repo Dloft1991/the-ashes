87bc3a12d41a69d923b3151d2db0ea34763355e0
Dloft1991 <derek.lofton1@gmail.com>
2021-04-26 11:48:00 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">From The Ashes</header>
+      <header className="App-header">From The Ashes!</header>
     </div>
   );
 }
